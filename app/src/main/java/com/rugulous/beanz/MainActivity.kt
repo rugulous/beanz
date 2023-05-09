ee18b848e5e3f40397181ab3ebff22dab7606c7f
Luke Taylor <luke@circleleasing.co.uk>
2023-05-09 14:17:03 +0100

    Initial commit

---
+++ app/src/main/java/com/rugulous/beanz/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.rugulous.beanz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        setContentView(R.layout.activity_main)
+    }
+}
