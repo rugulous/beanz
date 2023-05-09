dfe49ee910b9c12c03bc83b3786c915bacc03fa6
Luke Taylor <luke@circleleasing.co.uk>
2023-05-09 15:12:05 +0100

    Navigate to new activity

---
+++ app/src/main/java/com/rugulous/beanz/AddAccountActivity.kt
@@ -0,0 +1,11 @@
+package com.rugulous.beanz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_account)
+    }
+}
--- app/src/main/java/com/rugulous/beanz/MainActivity.kt
+++ app/src/main/java/com/rugulous/beanz/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.rugulous.beanz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Window
 import android.view.WindowManager
+
+private const val DELAY_TIMER = 3000L
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
-
         setContentView(R.layout.activity_main)
+
+        delayedOpenNextActivity();
+    }
+
+    private fun delayedOpenNextActivity(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, AddAccountActivity::class.java))
+        }, DELAY_TIMER)
     }
 }
