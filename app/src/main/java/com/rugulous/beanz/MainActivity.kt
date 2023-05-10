6cfc9a58736da85ffbe6bc370bcd0f6d2592b515
Luke Taylor <luke@circleleasing.co.uk>
2023-05-10 15:04:31 +0100

    Touch up theme somewhat

--- app/src/main/java/com/rugulous/beanz/MainActivity.kt
+++ app/src/main/java/com/rugulous/beanz/MainActivity.kt
@@ -7,16 +7,26 @@ import android.os.Handler
 import android.os.Looper
 import android.view.Window
 import android.view.WindowManager
+import androidx.core.content.ContextCompat
 
 private const val DELAY_TIMER = 3000L
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        initWindow()
         setContentView(R.layout.activity_main)
 
         delayedOpenNextActivity();
+    }
+
+    private fun initWindow(){
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.money)
     }
 
     private fun delayedOpenNextActivity(){
